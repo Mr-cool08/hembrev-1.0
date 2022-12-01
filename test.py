19cf943d889b4f3a287a17bf2f8ff82ceeb10d97
Liam Suorsa <liam@suorsa.se>
2022-12-01 08:58:53 +0100

    fixed the exe file and doing some testing

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-import urllib.request
-url = "https://github.com/Mr-cool08/hembrev/raw/master/hembrev.exe"
-print ("download start!")
-filename, headers = urllib.request.urlretrieve(url, filename="/program/hembrev.exe")
-print ("download complete!")
-print ("download file location: ", filename)
-print ("download headers: ", headers)
+import requests
+URL = ""
+response = requests.get(URL)
+open("instagram.ico", "wb").write(response.content)

72dfa5f0239e71376008835eba9f2a3d7f6316cc
Liam Suorsa <liamsuorsa08@gmail.com>
2023-01-02 13:43:49 +0100

    made a uninstall script

--- uninstall.py
+++ uninstall.py
@@ -1,5 +1,5 @@
 
-
+import winshell
 import os, shutil
 user = os.getlogin()
 folder = fr'C:\Users\{user}\AppData\Roaming\hembrev'
@@ -12,3 +12,15 @@ for filename in os.listdir(folder):
             shutil.rmtree(file_path)
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
+folder = fr'C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/hembrev'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+desktop = winshell.desktop()
+os.remove(f"{desktop}\hembrev.ink")
